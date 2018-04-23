d2b9e4c1c8a189541d511bd03b1cc423a51aa886
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-04-23 14:01:23 +0200

    non-leaf start

--- tests/iterator.rs
+++ tests/iterator.rs
@@ -12,3 +12,15 @@ fn iterator() {
   assert_eq!(iterator.next(), Some(13));
   assert_eq!(iterator.left_span(), 12);
 }
+
+#[test]
+fn non_leaf_start() {
+  let mut iterator = flat_tree::Iterator::new(1);
+  assert_eq!(iterator.index(), 1);
+  assert_eq!(iterator.parent(), 3);
+  assert_eq!(iterator.parent(), 7);
+  assert_eq!(iterator.right_child(), 11);
+  assert_eq!(iterator.left_child(), 9);
+  assert_eq!(iterator.next(), Some(13));
+  assert_eq!(iterator.left_span(), 12);
+}
