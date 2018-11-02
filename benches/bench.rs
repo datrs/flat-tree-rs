e6cb8b0723d35fa4915b7571c9cd334ed54ff8e6
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-11-02 14:18:19 +0100

    comment out full_roots tests
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,21 +9,18 @@ fn criterion_benchmark(c: &mut Criterion) {
   c.bench_function("children_256", |b| b.iter(|| flat_tree::children(256)));
   c.bench_function("count_128", |b| b.iter(|| flat_tree::count(128)));
   c.bench_function("depth_128", |b| b.iter(|| flat_tree::depth(128)));
-
-  c.bench_function("full_roots_16", |b| {
-    let mut nodes: Vec<usize> = Vec::with_capacity(16);
-    b.iter(|| {
-      flat_tree::full_roots(0, &mut nodes);
-    })
-  });
-
-  c.bench_function("full_roots_256", |b| {
-    let mut nodes: Vec<usize> = Vec::with_capacity(16);
-    b.iter(|| {
-      flat_tree::full_roots(256, &mut nodes);
-    })
-  });
-
+  // c.bench_function("full_roots_16", |b| {
+  //   let mut nodes: Vec<usize> = Vec::with_capacity(16);
+  //   b.iter(|| {
+  //     flat_tree::full_roots(16, &mut nodes);
+  //   })
+  // });
+  // c.bench_function("full_roots_256", |b| {
+  //   let mut nodes: Vec<usize> = Vec::with_capacity(16);
+  //   b.iter(|| {
+  //     flat_tree::full_roots(256, &mut nodes);
+  //   })
+  // });
   c.bench_function("index_3_1", |b| b.iter(|| flat_tree::index(3, 1)));
   c.bench_function("left_child_128", |b| b.iter(|| flat_tree::left_child(128)));
   c.bench_function("left_span_128", |b| b.iter(|| flat_tree::left_span(128)));
